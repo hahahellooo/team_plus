c8ea92b2b5bdc303697de80cea365f51b5a6c4c5
hahahellooo <hahahello777@gmail.com>
2024-07-17 10:22:02 +0900

    sys

--- src/team_plus/team_plus.py
+++ src/team_plus/team_plus.py
@@ -1,3 +1,15 @@
-def plus():
-    print(1+1)
+import sys
+
+
+
+def plus(a, b):
+    print(a+b)
+def main():
+
+    a =int(sys.argv[1])
+    b =int(sys.argv[2])
+    plus(a, b)
+if __name__ == "__main__":
+    main()
+
 
